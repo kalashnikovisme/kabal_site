fc92d8e953deb7715547bbd8d61c706b7553dafd
pavel <kalashnikovisme@gmail.com>
2014-06-23 20:41:18 +0400

    add locals for languages

--- lib/kabal_helper.rb
+++ lib/kabal_helper.rb
@@ -3,6 +3,10 @@ require 'kabal'
 module KabalHelper
   include Kabal
   def kabal_support_languages
-    supported_languages
+    languages_local = []
+    supported_languages.each do |language|
+      languages_local << I18n.t("languages.#{language}")
+    end
+    languages_local
   end
 end
