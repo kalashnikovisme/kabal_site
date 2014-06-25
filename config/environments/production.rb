2132848ec96660c124c9e67c4290a8c52b8670fa
pavel <kalashnikovisme@gmail.com>
2014-06-25 12:18:54 +0400

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,10 @@ KabalSite::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.compress = true
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.react.variant = :production
+  config.react.addons = true
 end
