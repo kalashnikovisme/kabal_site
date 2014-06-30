c88bc6ad2cb4d996c4af7fa43f9de95bb704d21f
pavel <kalashnikovisme@gmail.com>
2014-07-01 02:48:05 +0400

    add session new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 KabalSite::Application.routes.draw do
   root to: "web/numbers#new"
 
+  get '/admin' => "web/admin/welcome#index"
+
   scope module: :web do
     resources :numbers, only: [ :new, :create, :show ]
     resources :comments, only: :create
-    resource :sessions, only: [:new, :create, :destroy]
+    resource :session, only: [:new, :create, :destroy]
     namespace :admin do
-      resources :welcome, only: :index
       resources :pages, except: :show
     end
   end
