c734aa409093104144939b99e9b2e470b05860a1
pavel <kalashnikovisme@gmail.com>
2014-07-01 04:37:12 +0400

    edit redirect

--- app/controllers/web/sessions_controller.rb
+++ app/controllers/web/sessions_controller.rb
@@ -7,7 +7,7 @@ class Web::SessionsController < Web::ApplicationController
     @user = User.find_by_login params[:user][:login]
     if @user && @user.authenticate(params[:user][:password])
       sign_in @user
-      redirect_to root_path, flash: :success
+      redirect_to admin_path, flash: :success
     else
       @user = User.new params[:user]
       flash[:notice] = t('.wrong_login')
