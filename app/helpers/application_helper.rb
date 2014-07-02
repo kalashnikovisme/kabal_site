f2d2e776f46a506a656be706bb70c4535c2ae4d8
pavel <kalashnikovisme@gmail.com>
2014-07-02 14:10:38 +0400

    add links to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include KabalHelper
+
+  def links
+    links = []
+    Page.where(navbar: :true).each do |page|
+      links << content_tag(:li, link_to(page.title, page_path(page.slug)))
+    end
+    links.join('\n')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20140702080242) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.text     "slug"
+    t.boolean  "navbar"
   end
 
   create_table "users", :force => true do |t|
