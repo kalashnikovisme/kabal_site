7e3a0f7706e69ef03852d17a4d506e44a50bc30c
pavel <kalashnikovisme@gmail.com>
2014-07-26 02:24:28 +0400

    add log when error

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -9,6 +9,7 @@ class Web::ApplicationController < ApplicationController
 
   if Rails.env.production?
     rescue_from ActionController::RoutingError, ActionView::MissingTemplate, ActiveRecord::RecordNotFound, NoMethodError do |exception|
+      Rails.logger.warn "ERROR MESSAGE: #{exception.message}"
       redirect_to not_found_errors_path
     end
   end
