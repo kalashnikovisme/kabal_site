21a435b7d4e7a78194b7c8db80c46314787b693c
pavel <kalashnikovisme@gmail.com>
2014-08-02 07:30:05 +0400

    add language to page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,8 @@
 class Page < ActiveRecord::Base
-  attr_accessible :body, :title, :slug, :navbar
+  attr_accessible :body, :title, :slug, :navbar, :language
 
   validates :body, presence: true
   validates :title, presence: true
   validates :slug, presence: true
+  validates :language, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140704075134) do
+ActiveRecord::Schema.define(:version => 20140802031136) do
 
   create_table "comments", :force => true do |t|
     t.text     "text"
@@ -31,10 +31,11 @@ ActiveRecord::Schema.define(:version => 20140704075134) do
   create_table "pages", :force => true do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
     t.text     "slug"
     t.boolean  "navbar"
+    t.text     "language",   :default => "ru"
   end
 
   create_table "users", :force => true do |t|
