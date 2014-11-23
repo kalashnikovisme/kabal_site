182be5eeba324783c000e5563795258ac4cc63d9
pavel <kalashnikovisme@gmail.com>
2014-11-23 16:44:05 +0300

    return language name

--- lib/locales_helper.rb
+++ lib/locales_helper.rb
@@ -1,6 +1,6 @@
 module LocalesHelper
   def language_by_abbr(abbr)
-    @languages = { ru: "Русский",
+    @languages = { ru: "Russian",
                    en: "English",
                    de: "Deutsch" }
     @languages[abbr]
