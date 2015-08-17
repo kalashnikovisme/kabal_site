f79ae084af8c29617ffe056a68b0103d8e56ad34
pavel <kalashnikovisme@gmail.com>
2015-08-17 20:59:50 +0300

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,27 @@ end
 
 module KabalSite
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.i18n.default_locale = :ru
-    I18n.enforce_available_locales = false
+    config.autoload_paths += Dir[
+      "#{config.root}/lib/**/"
+    ]
+    config.active_record.raise_in_transactional_callbacks = true
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.encoding = "utf-8"
-    config.filter_parameters += [:password]
-    config.active_support.escape_html_entities_in_json = true
-    config.active_record.whitelist_attributes = true
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "*.ico")
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.generators do |g|
-      g.fixture_replacement :factory_girl
+      g.template_engine :haml
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.decorator false
     end
+    config.time_zone = 'Moscow'
     ActionDispatch::Reloader.to_prepare do
       load Rails.root.join('lib/configus.rb')
     end
