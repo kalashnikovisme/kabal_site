103ba7abd7041c9f28f78185694c5ec0542c4123
pavel <kalashnikovisme@gmail.com>
2015-08-17 20:53:26 +0300

    fix heroku bug

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module KabalSite
     config.active_support.escape_html_entities_in_json = true
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
     config.assets.version = '1.0'
     config.generators do |g|
       g.fixture_replacement :factory_girl
